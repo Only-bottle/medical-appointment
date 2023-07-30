d845e4a2835ca02200f7b6f44c50dfe25ce4ccf0
Only-bottle <bmlee@nota.ai>
2023-07-31 01:03:51 +0900

    [ADD] patients app 추가

---
+++ patients/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Patient
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "created_at",
+    )
---
+++ patients/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Patient(CommonModel):
+
+    """Model Definition for Patient"""
+
+    name = models.CharField(max_length=140)
+
+    def __str__(self) -> str:
+        return self.name
